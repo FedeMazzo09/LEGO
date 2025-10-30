33a587064745f4a018c40a9ae237955677accdbe
MAZZOLENI.22138 <mazzoleni.federico.studente@itispaleocapa.it>
2025-10-30 09:00:27 +0100

    Boh riguarda tutto

--- Mattoncino.java
+++ Mattoncino.java
@@ -1,37 +1,61 @@
 public class Mattoncino {
-    int Red;
-    int Green;
-    int Blue;
-    int Peso;
-    int larghezza;
-    int lunghezza;
-    int altezza;
+    private int codice;
+    private int red;
+    private int green;
+    private int blue;
+    private int peso;
+    private int larghezza;
+    private int lunghezza;
+    private int altezza;
+    private Mattoncino[] mattonciniAll;
 
-    public void setRed(int red) {
-        Red = red;
-    }
-
-    public void setGreen(int green) {
-        Green = green;
-    }
-
-    public void setBlue(int blue) {
-        Blue = blue;
-    }
-
-    public void setPeso(int peso) {
-        Peso = peso;
-    }
-
-    public void setLarghezza(int larghezza) {
+    public void Mattoncino(int codice, int red, int green, int blue, int peso, int larghezza, int lunghezza, int altezza) {
+        this.codice = codice;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.peso = peso;
         this.larghezza = larghezza;
-    }
-
-    public void setLunghezza(int lunghezza) {
         this.lunghezza = lunghezza;
-    }
-
-    public void setAltezza(int altezza) {
         this.altezza = altezza;
     }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public int getLarghezza() {
+        return larghezza;
+    }
+
+    public int getLunghezza() {
+        return lunghezza;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    /* public void  salvaMattoncini (){
+        mattonciniAll = new Mattoncino[10];
+        for (int i=0; i< mattonciniAll.length; i++){
+            if (mattonciniAll[i].getLarghezza()=)
+        }
+    }*/
 }
--- Prodotto.java
+++ Prodotto.java
@@ -1,2 +1,17 @@
 public class Prodotto {
+    private int codice;
+    private String nome;
+    private Mattoncino[] mattonciniProduct;
+
+    public Prodotto (int codice, String nome){
+        this.codice=codice;
+        this.nome=nome;
+        mattonciniProduct = new Mattoncino[20];
+    }
+
+    public void aggingiMattoncino (){
+        for (int i=0; i< mattonciniProduct.length; i++){
+            mattonciniProduct[i] =
+        }
+    }
 }
