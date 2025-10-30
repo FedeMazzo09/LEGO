5e8ba27181eb8fcbe4373f59bff9266a3aa07a4d
MAZZOLENI.22138 <mazzoleni.federico.studente@itispaleocapa.it>
2025-10-30 08:28:39 +0100

    Getter e Setter

---
+++ Mattoncino.java
@@ -0,0 +1,37 @@
+public class Mattoncino {
+    int Red;
+    int Green;
+    int Blue;
+    int Peso;
+    int larghezza;
+    int lunghezza;
+    int altezza;
+
+    public void setRed(int red) {
+        Red = red;
+    }
+
+    public void setGreen(int green) {
+        Green = green;
+    }
+
+    public void setBlue(int blue) {
+        Blue = blue;
+    }
+
+    public void setPeso(int peso) {
+        Peso = peso;
+    }
+
+    public void setLarghezza(int larghezza) {
+        this.larghezza = larghezza;
+    }
+
+    public void setLunghezza(int lunghezza) {
+        this.lunghezza = lunghezza;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+}
